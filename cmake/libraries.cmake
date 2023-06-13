81beb47ee2ecd18ee9a9c29d7feaac8003b3fd87
aCube <player559001@gmail.com>
2023-06-13 20:02:53 -0300

    Change SDL2 library dependency to SFML 2.5

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -2,17 +2,23 @@ include_guard()
 
 set(LIBS_DIR ${PROJECT_SOURCE_DIR}/libs)
 
-set(SDL2_VERSION 2.24.2)
 
 function(link_default_libraries target)
-	find_package(SDL2 ${SDL2_VERSION} REQUIRED)
+	find_package(
+		SFML 2.5.1
+		COMPONENTS
+			window graphics audio
+		REQUIRED
+	)
 
 	add_subdirectory(${LIBS_DIR}/spdlog)
 
 	target_link_libraries(
 		${target}
 		PRIVATE
-			SDL2::SDL2
 			spdlog::spdlog
+			sfml-window
+			sfml-graphics
+			sfml-audio
 	)
 endfunction()
